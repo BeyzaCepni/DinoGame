99e7808e62aa38a0286d50bf5a3b128df4b56b6f
beyca16 <f.beyza0216@gmail.com>
2024-05-05 03:50:40 +0300

    Game Over mechanisme done
    Gome Over and Start conditions are activated.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     public float gameSpeedIncrease = 0.1f;
     public float initialGameSpeed = 5f;
     public float gameSpeed {get; private set; }
+    private Player player;
+    private Spawner spawner;
 
     private void Awake()
     {
@@ -32,12 +34,33 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
+
         NewGame();
     }
 
     private void NewGame()
     {
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach (var obstacle in obstacles) {
+            Destroy(obstacle.gameObject);
+        }
+
         gameSpeed = initialGameSpeed;
+        enabled = true;
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
     }
 
     private void Update()
